4e9a0d340b18b1d16507bb6c1789fc4cbe4589a4
yurikon <h6606797@gmail.com>
2026-05-15 22:30:57 +0800

    feat: 修改falke以使用mac

--- flake.nix
+++ flake.nix
@@ -15,34 +15,50 @@
         "aarch64-darwin"
       ];
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
-
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          f pkgs
+        );
     in
     {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            gcc
-            gnumake
-            flex
-            bison
-
-            gdb
-            valgrind
+      devShells = forAllSystems (
+        pkgs:
+        let
+          linuxOnlyPackages = pkgs.lib.optionals pkgs.stdenv.isLinux [
+            pkgs.gdb
+            pkgs.valgrind
           ];
+        in
+        {
+          default = pkgs.mkShell {
+            packages =
+              [
+                pkgs.gcc
+                pkgs.gnumake
+                pkgs.flex
+                pkgs.bison
+              ]
+              ++ linuxOnlyPackages;
 
-          shellHook = ''
-            echo "C-- compiler lab environment"
-            echo "gcc:  $(gcc --version | head -n 1)"
-            echo  "make: $(make --version | head -n 1)"
-            echo  "flex: $(flex --version)"
-            echo  "bison: $(bison --version | head -n 1)"
-            echo ""
-            echo "Build with:"
-            echo "  make"
-            echo ""
-          '';
-        };
-      });
+            shellHook = ''
+              echo "C-- compiler lab environment"
+              echo "system: ${pkgs.stdenv.hostPlatform.system}"
+              echo "gcc:   $(gcc --version | head -n 1)"
+              echo "make:  $(make --version | head -n 1)"
+              echo "flex:  $(flex --version)"
+              echo "bison: $(bison --version | head -n 1)"
+              echo ""
+              echo "Build with:"
+              echo "  make"
+              echo ""
+            '';
+          };
+        }
+      );
     };
 }
